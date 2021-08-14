2382ba8f89a06b6b24f5a59e094fc555a5c9a8a9
LtVaios <vaioslt123@gmail.com>
2021-08-14 14:13:37 +0300

    admin homepage v1 + css updates on some files

--- src/app/admin-homepage/admin-homepage.component.ts
+++ src/app/admin-homepage/admin-homepage.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
+import {User} from "../model/user";
+import {AdminHomepageService} from "./admin-homepage.service";
+import {Likes} from "../model/likes";
+import {Friends} from "../model/friends";
+import {Post} from "../model/post";
+import {Job} from "../model/job";
+import * as JsonToXML from "js2xmlparser";
 
 @Component({
   selector: 'app-admin-homepage',
@@ -6,10 +14,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-homepage.component.css']
 })
 export class AdminHomepageComponent implements OnInit {
+  dataLoaded:boolean
+  downloadJsonHref:SafeUrl
+  user: User
+  likes: Likes[]
+  friends: Friends[]
+  comments: Comment[]
+  posts: Post[]
+  jobs: Job[]
+  users: User[]
+  jsons: Map<number,SafeUrl>
+  xmls: Map<number,any>
 
-  constructor() { }
+  constructor(private service: AdminHomepageService,private sanitizer: DomSanitizer) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.jsons=new Map<number,any>()
+    this.xmls=new Map<number,any>()
+    await this.loadUsers()
+    await this.load_JSONSand_XMLS()
+  }
+
+  async loadUsers(){
+    this.dataLoaded=false
+    await this.service.getUsers().toPromise().then(response=>this.users=response)
+  }
+
+
+  async load_JSONSand_XMLS() {
+    this.dataLoaded = false
+    var id:number;
+    for (let u of this.users){
+      id=u.id;
+      await this.service.getUser(id).toPromise().then(response => this.user = response)
+      await this.service.getUserComments(id).toPromise().then(response => this.comments = response)
+      await this.service.getPosts(id).toPromise().then(response => this.posts = response)
+      await this.service.getFriends(id).toPromise().then(response => this.friends = response)
+      await this.service.getJobs(id).toPromise().then(response => this.jobs = response)
+      await this.service.getUserLikes(id).toPromise().then(response => this.likes = response)
+      var combined=new Array()
+      combined.push(this.user)
+      combined.push(this.friends)
+      combined.push(this.posts)
+      combined.push(this.jobs)
+      combined.push(this.comments)
+      combined.push(this.likes)
+      var theJSON = JSON.stringify(combined);
+      var uri = this.sanitizer.bypassSecurityTrustUrl("data:text/json;charset=UTF-8," + encodeURIComponent(theJSON));
+      this.jsons.set(id, uri)
+
+      var obj=JsonToXML.parse("user", this.user)
+      var url = this.sanitizer.bypassSecurityTrustUrl("data:text/xml;charset=UTF-8," + encodeURIComponent(obj));
+      this.xmls.set(id,url)
+    }
+    this.dataLoaded=true
   }
 
 }
--- src/app/admin-homepage/admin-homepage.service.ts
+++ src/app/admin-homepage/admin-homepage.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import {User} from '../model/user';
+import { Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Friends} from "../model/friends";
+import {Post} from "../model/post";
+import {Likes} from "../model/likes";
+import {Job} from "../model/job";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminHomepageService {
+  private postsUrl = 'http://localhost:8080/posts';
+  private likesUrl = 'http://localhost:8080/likes';
+  private usersUrl = 'http://localhost:8080/users';
+  private friendsUrl = 'http://localhost:8080/friends';
+  private commentsUrl = 'http://localhost:8080/comments';
+  private jobsUrl = 'http://localhost:8080/jobs';
+  constructor( private http: HttpClient ) { }
 
-  constructor() { }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
+
+  getUser(user_id: number): Observable<User>{
+    return this.http.get<User>(this.usersUrl+"/getbyid/"+user_id);
+  }
+
+  getFriends(user_id:number): Observable<Friends[]> {
+    return this.http.get<Friends[]>(this.friendsUrl+ '/' + user_id);
+  }
+
+  getPosts(user_id:number): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postsUrl+"/ofuser/"+user_id);
+  }
+
+  getUserLikes(user_id:number): Observable<Likes[]>{
+    return this.http.get<Likes[]>(this.likesUrl+"/ofuser/"+user_id);
+  }
+
+  getUserComments(user_id:number): Observable<Comment[]>{
+    return this.http.get<Comment[]>(this.commentsUrl+"/ofuser/"+user_id);
+  }
+
+  getJobs(user_id:number): Observable<Job[]> {
+    return this.http.get<Job[]>(this.jobsUrl+"/"+user_id);
+  }
+
 }
